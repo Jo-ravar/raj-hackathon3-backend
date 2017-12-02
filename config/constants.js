15ff2ebc1ddffce2b380b9cd5a7577c0386f7be5
Jo-ravar <ravijoshi0996@gmail.com>
2017-12-02 10:38:57 +0530

    Changed Css colors

--- config/constants.js
+++ config/constants.js
@@ -2,7 +2,9 @@ module.exports = {
   MONGO_DB: {
     URI:
       process.env.MONGODB_URI ||
-      'mongodb://walker:rajasthan@ds155582.mlab.com:55582/hackathon3',
+      //'mongodb://hack2:qwerty@ds127506.mlab.com:27506/hackathon3',
+      //'mongodb://walker:rajasthan@ds155582.mlab.com:55582/hackathon3',
+      'mongodb://localhost:27017/hackathon3',
   },
   OTP_AUTH_KEY: '186133AyQXQfeIYD5a1fb8a3',
   SENDER_ID: 'RAJGOV',
